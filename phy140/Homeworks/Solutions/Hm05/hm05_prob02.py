91ef5dc4fb84bf4983f55eee623b13f334a4a6d9
Ptochos.Fotios@ucy.ac.cy <Ptochos.Fotios@ucy.ac.cy>
2023-11-27 17:39:23 +0200

    Prob2 set5

--- phy140/Homeworks/Solutions/Hm05/hm05_prob02.py
+++ phy140/Homeworks/Solutions/Hm05/hm05_prob02.py
@@ -5,7 +5,13 @@ import numpy as np
 import matplotlib.pyplot as plt
 
 def f(x):
-    return 6*np.cos(x) - x*x - 4
+    return 6*np.cos(x) + x*x - 4
+
+def d1x(x):
+    return -6*np.sin(x) + 2*x
+
+def d2x(x):
+    return -6*np.cos(x) + 2
 
 def plotfunc():
     x = [-4 + iv*0.01 for iv in range(0,801)]   # Sto diastima [-4 4]
@@ -47,15 +53,38 @@ def bisection(f, x_L, x_R, eps, return_x_list=False):
     else:
         return x_M, iteration_counter
 
+def Newton(f, df, lowl, uppl, eps):
+    x = lowl
+    f_value = f(x)
+    iteration_counter = 0
+
+    while abs(f_value) > eps and iteration_counter < 100:
+        try:
+            x = x - f_value/df(x)
+        except:
+            print("Error! - derivative zero for x = ", x)
+            sys.exit(1)     # Abort with error
+
+        f_value = f(x)
+        iteration_counter += 1
+
+    if abs(f_value) > eps:
+        iteration_counter = -1  # i.e., lack of convergence
+        
+    return x, iteration_counter
+
 
 plotfunc()
 
 eps=float(input("Eisagete tin epithumiti akriveia sti lusi [eps=1E-4] "))
 
-intervals = [[0.5,1.0],[1.5,2.0]]       # Duo luseis - ara 2 diastimata
+intervals = [[-1.0,1.3],[1.5,3.5]]      # Duo luseis - ara 2 diastimata
 sols = []                               # list gia apothikeusi twn lusewm
 
 for xval_L, xval_R in intervals:
     sol, niters = bisection(f,xval_L, xval_R, eps)
     sols.append(sol)
     print("Solution%d is at x = %6.4f"%(len(sols),sol))
+
+mx,nit = Newton(d1x, d2x, -0.5, 0.5, eps)
+print("The function has a maximum at x = %6.4f with a value of y=%6.4f"%(mx,f(mx) ))
